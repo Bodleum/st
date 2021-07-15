4132c3f81f5d3ee3700debba03aeffaff786e1ae
Bodleum <git@daniellaing.com>
2021-07-15 10:56:37 +0100

    Colour scheme tweaks

--- config.h
+++ config.h
@@ -121,14 +121,14 @@ static const char *colorname[] = {
 	"#b16286",
 	"#689d6a",
 	"#a89984",
-	"#928374",
+	"#5e544a",
 	"#fb4934",
 	"#b8bb26",
 	"#fabd2f",
 	"#83a598",
 	"#d3869b",
 	"#8ec07c",
-	"#ebdbb2",
+	"#f8f2e3",
 	[255] = 0,
 	/* more colors can be added after 255 to use with DefaultXX */
 	"#add8e6", /* 256 -> cursor */
